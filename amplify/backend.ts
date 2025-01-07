4a377fc9e91225b1b76a3d93d812474fbf6bc10f
thom krupa <thom@bejamas.com>
2025-01-07 22:42:35 +0100

    feat: add function url

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,17 @@
 import { defineBackend } from "@aws-amplify/backend";
+import { Function, FunctionUrlAuthType } from "aws-cdk-lib/aws-lambda";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { helloWorld } from "./hello-world/resource";
 
-defineBackend({
+const backend = defineBackend({
 	auth,
 	data,
 	helloWorld,
+});
+
+const myFunc = backend.helloWorld.resources.lambda as Function;
+
+myFunc.addFunctionUrl({
+	authType: FunctionUrlAuthType.AWS_IAM,
 });
